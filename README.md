1da61e9c218841d58139e22f82cf20375bd68f8c
Navleshkumar-Yadav <kumarnavlesh604@gmail.com>
2025-05-05 14:22:32 +0530

    Added basic routing layer

--- README.md
+++
@@ -1,3 +0,0 @@
-https://medium.com/@sindhujad6/setting-up-eslint-and-prettier-in-a-node-js-project-f2577ee2126f
-
-elint setup
---
+++ src/config/dbConfig.js
@@ -0,0 +1,29 @@
+
+import mongoose from 'mongoose';
+
+import { DEV_DB_URL, NODE_ENV, PROD_DB_URL } from './serverConfig.js';
+export default async function connectDB(){
+        try{    // testing and development
+                if(NODE_ENV == 'development'){
+                        await mongoose.connect(DEV_DB_URL);
+
+                } 
+                // deployment 
+                else if (NODE_ENV == 'production'){
+                        await mongoose.connect(PROD_DB_URL);
+                }
+                console.log(`Connected to mongodb database from ${NODE_ENV} environment`);
+
+                }
+                catch(error){
+                        console.log('Error connecting to database',error);
+
+
+         }
+ }
+
+
+
+
+
+
